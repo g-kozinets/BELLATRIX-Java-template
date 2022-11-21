81db314681bdba832e456755807bbed794cf88d6
g.kozinets <glebokator@gmail.com>
2022-11-21 11:06:22 +0300

    deleted unused tests added circle ci config

---
+++ bellatrix.api.tests/src/main/java/solutions/bellatrix/Main.java
@@ -0,0 +1,7 @@
+package solutions.bellatrix;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ bellatrix.api.tests/src/test/java/apitests/MyApiTests.java
@@ -0,0 +1,8 @@
+package apitests;
+
+import solutions.bellatrix.api.infrastructure.junit.ApiTest;
+
+public class MyApiTests extends ApiTest {
+
+    
+}
